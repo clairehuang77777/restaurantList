ad6c6e0b1f3a8bf467c4b4bbfa8ef819ac20d88c
huang <clairehuang77777@gmail.com>
2024-07-30 18:50:55 +0800

    build routing & add static file

--- app.js
+++ app.js
@@ -4,14 +4,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+//建立靜態routing
+app.use(express.static('public'));
+
+
+//建立動態routing
 app.get('/', (req, res) => {
   res.send('Hello its a restaurant page!')
 })
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`This is the restaurant ${id} details`)
+})
+
+app.get('/restaurant', (req, res) => {
+  const keyword = req.query.keyword
+  res.send(`This is ${keyword} search result`)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 
-//建立routing
 
