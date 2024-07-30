0978ba0510b912543508eb1ee226f48c39f15dd3
huang <clairehuang77777@gmail.com>
2024-07-30 20:49:51 +0800

    build express-handler as template engine

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 
 //建立本機伺服器 local server 
 const express = require('express')
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+//初始化express-handlebars
 const app = express()
 const port = 3000
+
+//初始化express-handlebars
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views',  './views');
+
 
 
 
@@ -11,8 +21,13 @@ app.use(express.static('public'));
 
 
 //建立動態routing
+//當輸入根目錄時，幫我轉導到/home, 並刺激hbs幫我render home.hbs
 app.get('/', (req, res) => {
-  res.send('Hello its a restaurant page!')
+  res.redirect('/home')
+})
+
+app.get('/home' , (req, res) => {
+  res.render('home')
 })
 
 app.get('/restaurant/:id', (req, res) => {
@@ -20,7 +35,7 @@ app.get('/restaurant/:id', (req, res) => {
   res.send(`This is the restaurant ${id} details`)
 })
 
-app.get('/restaurant', (req, res) => {
+app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   res.send(`This is ${keyword} search result`)
 })
